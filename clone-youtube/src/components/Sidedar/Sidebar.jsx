6eeb43c63972f30409661575451e0e0f06bcab2c
Yuttasin-Phokaew <71009316+Yuttasin-Phokaew@users.noreply.github.com>
2024-02-07 18:37:57 +0700

    add sidebar complete

--- clone-youtube/src/components/Sidedar/Sidebar.jsx
+++ clone-youtube/src/components/Sidedar/Sidebar.jsx
@@ -1,7 +1,240 @@
 import React from 'react'
-
+import './SidebarStyle.css'
 export default function Sidebar() {
   return (
-    <section id='side'>Sidebar</section>
+    <section id='side'>
+      <div className='-side-pmr'>
+        <a href='#!' className='-item'>
+          <div className='-icon'>
+            <img src='https://via.placeholder.com/24x24' alt=''/>
+          </div>
+          <div className='-text'>หน้าแรก</div>
+        </a>
+      </div>
+      <div className='-side-pmr'>
+        <a href='#!' className='-item'>
+          <div className='-icon'>
+            <img src='https://via.placeholder.com/24x24' alt=''/>
+          </div>
+          <div className='-text'>Shorts</div>
+        </a>
+      </div>
+      <div className='-side-pmr'>
+        <a href='#!' className='-item'>
+          <div className='-icon'>
+            <img src='https://via.placeholder.com/24x24' alt=''/>
+          </div>
+          <div className='-text'>การติดตาม</div>
+        </a>
+      </div>
+      <div className='-side-pmr'>
+        <a href='#!' className='-item'>
+          <div className='-icon'>
+            <img src='https://via.placeholder.com/24x24' alt=''/>
+          </div>
+          <div className='-text'>YouTube Music</div>
+        </a>
+      </div>
+      <div className='-side-pmr'>
+        <a href='#!' className='-item'>
+          <div className='-icon'>
+            <img src='https://via.placeholder.com/24x24' alt=''/>
+          </div>
+          <div className='-text'>คุณ</div>
+        </a>
+      </div>
+      <div className='-side-pmr'>
+        <a href='#!' className='-item'>
+          <div className='-icon'>
+            <img src='https://via.placeholder.com/24x24' alt=''/>
+          </div>
+          <div className='-text'>ช่องของคุณ</div>
+        </a>
+      </div>
+      <div className='-side-pmr'>
+        <a href='#!' className='-item'>
+          <div className='-icon'>
+            <img src='https://via.placeholder.com/24x24' alt=''/>
+          </div>
+          <div className='-text'>ประวัติการเข้าชม</div>
+        </a>
+      </div>
+      <div className='-side-pmr'>
+        <a href='#!' className='-item'>
+          <div className='-icon'>
+            <img src='https://via.placeholder.com/24x24' alt=''/>
+          </div>
+          <div className='-text'>วิดีโอของคุณ</div>
+        </a>
+      </div>
+      <div className='-side-pmr'>
+        <a href='#!' className='-item'>
+          <div className='-icon'>
+            <img src='https://via.placeholder.com/24x24' alt=''/>
+          </div>
+          <div className='-text'>ดูภายหลัง</div>
+        </a>
+      </div>
+      <div className='-side-pmr'>
+        <a href='#!' className='-item'>
+          <div className='-icon'>
+            <img src='https://via.placeholder.com/24x24' alt=''/>
+          </div>
+          <div className='-text'>การดาวโหลด</div>
+        </a>
+      </div>
+      <div className='-side-pmr'>
+        <a href='#!' className='-item'>
+          <div className='-icon'>
+            <img src='https://via.placeholder.com/24x24' alt=''/>
+          </div>
+          <div className='-text'>วิดีโอที่ชอบ</div>
+        </a>
+      </div>
+      <div className='-side-pmr'>
+        <a href='#!' className='-item'>
+          <div className='-icon'>
+            <img src='https://via.placeholder.com/24x24' alt=''/>
+          </div>
+          <div className='-text'>การดาวโหลด</div>
+        </a>
+      </div>
+      <div className='-side-pmr'>
+        <a href='#!' className='-item'>
+          <div className='-icon'>
+            <img src='https://via.placeholder.com/24x24' alt=''/>
+          </div>
+          <div className='-text'>การดาวโหลด</div>
+        </a>
+      </div>
+      <div className='-side-pmr'>
+        <a href='#!' className='-item'>
+          <div className='-icon'>
+            <img src='https://via.placeholder.com/24x24' alt=''/>
+          </div>
+          <div className='-text'>การดาวโหลด</div>
+        </a>
+      </div>
+      <div className='-side-pmr'>
+        <a href='#!' className='-item'>
+          <div className='-icon'>
+            <img src='https://via.placeholder.com/24x24' alt=''/>
+          </div>
+          <div className='-text'>การดาวโหลด</div>
+        </a>
+      </div>
+      <div className='-side-pmr'>
+        <a href='#!' className='-item'>
+          <div className='-icon'>
+            <img src='https://via.placeholder.com/24x24' alt=''/>
+          </div>
+          <div className='-text'>การดาวโหลด</div>
+        </a>
+      </div>
+      <div className='-side-pmr'>
+        <a href='#!' className='-item'>
+          <div className='-icon'>
+            <img src='https://via.placeholder.com/24x24' alt=''/>
+          </div>
+          <div className='-text'>การดาวโหลด</div>
+        </a>
+      </div>
+      <div className='-side-pmr'>
+        <a href='#!' className='-item'>
+          <div className='-icon'>
+            <img src='https://via.placeholder.com/24x24' alt=''/>
+          </div>
+          <div className='-text'>การดาวโหลด</div>
+        </a>
+      </div>
+      <div className='-side-pmr'>
+        <a href='#!' className='-item'>
+          <div className='-icon'>
+            <img src='https://via.placeholder.com/24x24' alt=''/>
+          </div>
+          <div className='-text'>การดาวโหลด</div>
+        </a>
+      </div>
+      <div className='-side-pmr'>
+        <a href='#!' className='-item'>
+          <div className='-icon'>
+            <img src='https://via.placeholder.com/24x24' alt=''/>
+          </div>
+          <div className='-text'>การดาวโหลด</div>
+        </a>
+      </div>
+      <div className='-side-pmr'>
+        <a href='#!' className='-item'>
+          <div className='-icon'>
+            <img src='https://via.placeholder.com/24x24' alt=''/>
+          </div>
+          <div className='-text'>การดาวโหลด</div>
+        </a>
+      </div>
+      <div className='-side-pmr'>
+        <a href='#!' className='-item'>
+          <div className='-icon'>
+            <img src='https://via.placeholder.com/24x24' alt=''/>
+          </div>
+          <div className='-text'>การดาวโหลด</div>
+        </a>
+      </div>
+      <div className='-side-pmr'>
+        <a href='#!' className='-item'>
+          <div className='-icon'>
+            <img src='https://via.placeholder.com/24x24' alt=''/>
+          </div>
+          <div className='-text'>การดาวโหลด</div>
+        </a>
+      </div>
+      <div className='-side-pmr'>
+        <a href='#!' className='-item'>
+          <div className='-icon'>
+            <img src='https://via.placeholder.com/24x24' alt=''/>
+          </div>
+          <div className='-text'>การดาวโหลด</div>
+        </a>
+      </div>
+      <div className='-side-pmr'>
+        <a href='#!' className='-item'>
+          <div className='-icon'>
+            <img src='https://via.placeholder.com/24x24' alt=''/>
+          </div>
+          <div className='-text'>การดาวโหลด</div>
+        </a>
+      </div>
+      <div className='-side-pmr'>
+        <a href='#!' className='-item'>
+          <div className='-icon'>
+            <img src='https://via.placeholder.com/24x24' alt=''/>
+          </div>
+          <div className='-text'>การดาวโหลด</div>
+        </a>
+      </div>
+      <div className='-side-pmr'>
+        <a href='#!' className='-item'>
+          <div className='-icon'>
+            <img src='https://via.placeholder.com/24x24' alt=''/>
+          </div>
+          <div className='-text'>การดาวโหลด</div>
+        </a>
+      </div>
+      <div className='-side-pmr'>
+        <a href='#!' className='-item'>
+          <div className='-icon'>
+            <img src='https://via.placeholder.com/24x24' alt=''/>
+          </div>
+          <div className='-text'>การดาวโหลด</div>
+        </a>
+      </div>
+      <div className='-side-pmr'>
+        <a href='#!' className='-item'>
+          <div className='-icon'>
+            <img src='https://via.placeholder.com/24x24' alt=''/>
+          </div>
+          <div className='-text'>การดาวโหลด</div>
+        </a>
+      </div>
+    </section>
   )
 }
